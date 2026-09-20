23de5f21575d38c866be1b90e6eccdec8132f11e
Vansh Rekhi <vanshrekhi05@gmail.com>
2026-09-20 17:28:11 +0530

    Connect frontend to Render API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 // Before Netlify deployment, replace this with: https://your-render-service.onrender.com/api
-const API = 'http://localhost:5000/api';
+const API = "https://cloud-computing-project1-add-delete.onrender.com/api";
 export default axios.create({ baseURL: API, headers: { 'Content-Type': 'application/json' } });
